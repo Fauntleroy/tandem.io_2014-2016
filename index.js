8311240fa9182c6fd98eb548e229ace274801848
Timothy Kempf <tim@kemp59f.info>
2014-02-08 13:11:16 -0800

    set up basic server side template solution set up basic assets solution

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+const PORT = 8080;
+
 var express = require('express');
 var server = express();
 
+// Set up templates for Express
+var express_handlebars = require('express3-handlebars');
+var handlebars = express_handlebars({
+	extname: '.hbs',
+	defaultLayout: false,
+});
+server.engine( 'hbs', handlebars );
+server.set( 'view engine', 'hbs' );
+
+// Static file serving
+server.use( express.static( __dirname + '/assets' ) );
+
 server.get( '/', function( req, res ){
-	res.send('quicksync');
+	res.render('index.hbs');
 });
 
-server.listen( 8080 );
+server.listen( PORT );
+
+console.log('[quicksync] listening on port', PORT );
