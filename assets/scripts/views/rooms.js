8bc62d0fb97f2bec456f14469939b7c5cb8ec532
Timothy Kempf <tim@kemp59f.info>
2014-02-09 18:42:21 -0800

    simplify template inclusion remove silly comment

--- assets/scripts/views/rooms.js
+++ assets/scripts/views/rooms.js
@@ -2,10 +2,10 @@ var Backbone = require('backbone');
 var $ = Backbone.$ = require('jquery');
 var handlebars = require('handlebars');
 
-var rooms_template = require('../../compiled/templates/rooms.js'); // don't like that traversal
+var rooms_template = require('../../compiled/templates/rooms.js')( handlebars );
 
 module.exports = Backbone.View.extend({
-	template: rooms_template( handlebars ),
+	template: rooms_template,
 	initialize: function(){
 		this.listenTo( this.collection, 'sync', this.render );
 		this.render();
