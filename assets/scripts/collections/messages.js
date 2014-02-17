d4d143b490a9f3be4d5bda0a5d4489d004a15aeb
Timothy Kempf <tim@kemp59f.info>
2014-02-16 16:31:31 -0800

    identify module data is coming from

--- assets/scripts/collections/messages.js
+++ assets/scripts/collections/messages.js
@@ -1,10 +1,18 @@
+var es = require('event-stream');
 var Backbone = require('backbone');
 
 module.exports = Backbone.Collection.extend({
 	initialize: function( models, config ){
 		this.mediator = config.mediator;
-		this.stream = config.stream;
+		var write_stream = es.through( this.preprocessStream );
+		write_stream.pipe( config.stream );
+		this.stream = es.duplex( write_stream, config.stream );
 		this.stream.on( 'data', this.processStream );
+	},
+	// Modify data object before sending to stream
+	preprocessStream: function( data ){
+		data.module = 'chat';
+		this.queue( data );
 	},
 	// Ensures stream data is properly routed
 	processStream: function( data ){
