9b61843c4b0f298044407c9a4bae6b6f67c960b3
Timothy Kempf <tim@kemp59f.info>
2014-02-16 16:54:23 -0800

    render incoming chat messages

--- assets/scripts/views/chat.js
+++ assets/scripts/views/chat.js
@@ -2,13 +2,20 @@ var Backbone = require('backbone');
 var $ = Backbone.$ = require('jquery');
 
 var chat_template = require('../../templates/chat.hbs');
+var message_template = require('../../templates/message.hbs');
 
 module.exports = Backbone.View.extend({
 	template: chat_template,
+	message_template: message_template,
 	initialize: function(){
 		this.render();
+		this.listenTo( this.collection, 'add', this.addMessage );
 	},
 	render: function(){
 		this.$el.html( this.template() );
+		this.$messages = this.$el.find('.messages');
+	},
+	addMessage: function( message ){
+		this.$messages.prepend( this.message_template( message.toJSON() ) );
 	}
 })
