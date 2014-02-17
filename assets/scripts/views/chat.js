8ec2eaabd0496fb955382f0648672f3bd059b17c
Timothy Kempf <tim@kemp59f.info>
2014-02-16 17:00:28 -0800

    enable sending of messages via interface

--- assets/scripts/views/chat.js
+++ assets/scripts/views/chat.js
@@ -7,15 +7,37 @@ var message_template = require('../../templates/message.hbs');
 module.exports = Backbone.View.extend({
 	template: chat_template,
 	message_template: message_template,
+	events: {
+		'submit form': 'submitMessage',
+		'keypress textarea[name="message"]': 'keyMessage'
+	},
 	initialize: function(){
 		this.render();
 		this.listenTo( this.collection, 'add', this.addMessage );
 	},
 	render: function(){
 		this.$el.html( this.template() );
-		this.$messages = this.$el.find('.messages');
+		this.$messages = this.$('.messages');
+		this.$new_message = this.$('.new_message');
+		this.$message = this.$('[name="message"]');
 	},
 	addMessage: function( message ){
 		this.$messages.prepend( this.message_template( message.toJSON() ) );
+	},
+	// send a new message when the form is submitted and a message exists
+	submitMessage: function( e ){
+		e.preventDefault();
+		var message = $.trim( this.$message.val() );
+		if( message ){
+			this.collection.sendMessage( message );
+			this.$message.val('');
+		}
+	},
+	// trigger form submit under the right conditions
+	keyMessage: function( e ){
+		if( e.which === 13 && !e.shiftKey ){
+			e.preventDefault();
+			this.$new_message.trigger('submit');
+		}
 	}
 })
