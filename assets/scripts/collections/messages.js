863b386e0533a45521a8c6c53f8a5d41263bb50c
Timothy Kempf <tim@kemp59f.info>
2014-02-16 16:46:51 -0800

    add method for sending messages

--- assets/scripts/collections/messages.js
+++ assets/scripts/collections/messages.js
@@ -1,8 +1,10 @@
 var es = require('event-stream');
+var _ = require('underscore');
 var Backbone = require('backbone');
 
 module.exports = Backbone.Collection.extend({
 	initialize: function( models, config ){
+		_( this ).bindAll( 'processStream' );
 		this.mediator = config.mediator;
 		var write_stream = es.through( this.preprocessStream );
 		write_stream.pipe( config.stream );
@@ -17,5 +19,23 @@ module.exports = Backbone.Collection.extend({
 	// Ensures stream data is properly routed
 	processStream: function( data ){
 		console.log( data );
+		if( data.module === 'chat' ){
+			switch( data.type ){
+			case 'message':
+				this.onMessage( data.payload );	
+			break;
+			}
+		}
+	},
+	sendMessage: function( message ){
+		this.stream.write({
+			type: 'message',
+			payload: {
+				content: message
+			}
+		});
+	},
+	onMessage: function( message ){
+		this.add( message );
 	}
 })
