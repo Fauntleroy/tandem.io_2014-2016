3469d542e6ca0744c5f9ce20eb93b50c3b20ae32
Timothy Kempf <tim@kemp59f.info>
2014-02-21 19:47:29 -0800

    use watchify to browserify assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,18 @@
 var spawn = require('child_process').spawn;
+var vinyl_source = require('vinyl-source-stream');
+var watchify = require('watchify');
 var gulp = require('gulp');
-var gulp_handlebars = require('gulp-handlebars');
-var gulp_browserify = require('gulp-browserify');
-var gulp_jshint = require('gulp-jshint');
-
-gulp.task( 'compile templates', function(){
-	gulp.src('./assets/templates/**/*.hbs')
-	.pipe( gulp_handlebars({
-		outputType: 'commonjs',
-		wrapped: true
-	}) )
-	.pipe( gulp.dest('./assets/compiled/templates') );
-});
 
 gulp.task( 'compile js', function(){
-	gulp.run('compile templates');
-	gulp.src('./assets/scripts/room.js')
-	.pipe( gulp_jshint() )
-	.pipe( gulp_jshint.reporter('default') )
-	.pipe( gulp_browserify({
-		transform: ['hbsfy']
-	}) )
-	.pipe( gulp.dest('./assets/compiled') );
-});
-
-gulp.task( 'watch', function(){
-	gulp.watch( ['./assets/scripts/**/*.js', './assets/templates/**/*.hbs'], ['compile js'] );
+	var w = watchify('./assets/scripts/room.js');
+	w.transform('hbsfy');
+	var bundle = function(){
+		return w.bundle()
+			.pipe( vinyl_source('room.js') )
+			.pipe( gulp.dest('./assets/compiled') );
+	};
+	w.on( 'update', bundle );
+	return bundle();
 });
 
 gulp.task( 'start application', function(){
@@ -34,4 +21,4 @@ gulp.task( 'start application', function(){
 	});
 });
 
-gulp.task( 'default', ['compile js', 'watch', 'start application'] );
+gulp.task( 'default', ['compile js', 'start application'] );
