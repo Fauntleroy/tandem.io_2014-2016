ccdd639c39abe83ed26aab9b9a761de356d16a21
Timothy Kempf <the.kaempfer@gmail.com>
2014-11-30 19:33:00 -0800

    bring browserify compilation scripts up to date

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,56 @@
-var spawn = require('child_process').spawn;
 var path = require('path');
 var vinyl_source = require('vinyl-source-stream');
+var _ = require('underscore');
+var browserify = require('browserify');
 var watchify = require('watchify');
 var gulp = require('gulp');
+var gulp_util = require('gulp-util');
 var gulp_less = require('gulp-less');
 var gulp_livereload = require('gulp-livereload');
 var gulp_uglify = require('gulp-uglify');
 var gulp_minify_css = require('gulp-minify-css');
 
 gulp.task( 'compile css', function(){
-	gulp.src('./assets/styles/index.less')
+	var css_stream = gulp.src('./assets/styles/index.less')
 		.pipe( gulp_less({
 			paths: [ path.join( __dirname, 'assets', 'styles' ) ]
 		}))
 		.pipe( gulp.dest('./assets/compiled') )
-		.pipe( gulp_livereload() );
+		css_stream.pipe( gulp_livereload() );
 });
 
+var generateBrowserifyBundler = function(){
+	var args = _.extend( {
+		transform: ['hbsfy', 'browserify-shim']
+	}, watchify.args );
+	var bundler = browserify( './assets/scripts/room.js', args );
+	return bundler;
+};
+
+var generateBrowserifyStream = function( bundler ){
+	var stream = bundler.bundle()
+		.on( 'error', gulp_util.log.bind( gulp_util, 'Browserify Error' ) )
+		.pipe( vinyl_source('room.js') )
+		.pipe( gulp.dest('./assets/compiled') );
+	return stream;
+};
+
 gulp.task( 'compile js', function(){
-	var w = watchify('./assets/scripts/room.js');
-	w.transform('hbsfy');
-	w.transform('browserify-shim');
-	var bundle = function(){
-		return w.bundle()
-			.pipe( vinyl_source('room.js') )
-			.pipe( gulp.dest('./assets/compiled') );
+	var bundler = generateBrowserifyBundler();
+	return generateBrowserifyStream( bundler );
+});
+
+gulp.task( 'compile and watch js', function(){
+	var bundler = watchify( generateBrowserifyBundler() );
+	var rebundle = function() {
+		console.log('[watchify] Bundling js...');
+		return generateBrowserifyStream( bundler );
 	};
-	w.on( 'update', bundle );
-	w.on( 'error', function(){
-		console.log('error', arguments);
-	})
-	return bundle();
+	bundler.on( 'update', rebundle );
+	bundler.on( 'time', function( bundle_time ){
+		console.log('[watchify] Bundled in '+ bundle_time +'ms');
+	});
+	return rebundle();
 });
 
 gulp.task( 'minify', function(){
