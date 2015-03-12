4f696502978bd96f6f7e9d0154d2e3177095ff9d
Timothy Kempf <the.kaempfer@gmail.com>
2015-03-11 21:05:15 -0700

    linkify message contents... poorly

--- assets/scripts/components/chat/ChatMessage.jsx
+++ assets/scripts/components/chat/ChatMessage.jsx
@@ -4,8 +4,30 @@ var cx = require('classnames');
 var User = require('../User.jsx');
 var Timestamp = require('./Timestamp.jsx');
 
+// from https://github.com/medialize/URI.js/blob/0cd4b1f09c7367d86e51731e692c3d20111f2681/src/URI.js#L191
+var url_regex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/ig;
+
+var _linkify = function( text ) {
+	// using split with this regex results in lots of extra undefineds, which are skipped later
+	// TODO this is horrible and should be refactored
+	var split_strings = text.split( url_regex );
+	var linkified_jsx = [];
+	split_strings.forEach( function( string ){
+		if( string ){
+			if( url_regex.test( string ) ){
+				linkified_jsx.push( <a href={string} target="_blank">{string}</a> );
+			}
+			else {
+				linkified_jsx.push( string );
+			}
+		}
+	});
+	return linkified_jsx;
+}
+
 var _generateContent = function( content ){
 	return content.map( function( message_content ){
+		message_content = _linkify( message_content );
 		return <p>{message_content}</p>;
 	});
 };
